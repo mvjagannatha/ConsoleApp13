99703f981849aca8aaa7edb9831e516b8070dab0
Admin <Admin@DESKTOP-6PQE64G>
2024-01-16 18:39:53 +0530

    Add project files.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Program
+{
+    
+    public static int secondLastDigitOf(int input1)
+    {
+       
+        input1 = Math.Abs(input1);
+
+        
+        if (input1 < 10)
+        {
+            return -1;
+        }
+        else
+        {
+           
+            return (input1 / 10) % 10;
+        }
+    }
+
+    static void Main()
+    {
+        
+        Console.WriteLine(secondLastDigitOf(197));  
+        Console.WriteLine(secondLastDigitOf(-197)); 
+        Console.WriteLine(secondLastDigitOf(5));    
+        Console.WriteLine(secondLastDigitOf(-5));   
+    }
+}
